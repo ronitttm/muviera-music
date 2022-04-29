8761e09f228f4c160c618b0687cd43d0ed38aa1b
ronitttm <89841303+ronitttm@users.noreply.github.com>
2022-04-29 19:57:34 +0530

    Update music.py

--- music.py
+++ music.py
@@ -29,7 +29,7 @@ st.markdown("""
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link disabled" href="http://127.0.0.1:8000/recommendation/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="https://share.streamlit.io/ronitttm/muviera-movie/main/movie.py" target="_blank">Movie</a>
